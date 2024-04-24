94427b8ea75389bcbc2549675011cf1016bcaef6
vjvikramgit <162918077+vjvikramgit@users.noreply.github.com>
2024-04-24 22:50:50 +0530

    Update ubuntu jenkins.sh

--- ubuntu jenkins.sh
+++ ubuntu jenkins.sh
@@ -10,4 +10,16 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+#Install Jenkins
 sudo apt-get install jenkins
+
+update-alternatives --config java
+
+#STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
+systemctl start jenkins.service
+systemctl status jenkins.service
+systemctl enable jenkins.service
+#Get Username & Password For Jenkins Server
+Username: Admin
+Get Password Cmd: cd /var/lib/jenkins/secrets/,  and after nano initialAdminPassword
+#mvn cmd for war generation: mvn clean package
