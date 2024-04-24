3ae583781fedea08b6a4c4383e0e1083ee098315
vjvikramgit <162918077+vjvikramgit@users.noreply.github.com>
2024-04-24 22:21:52 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -18,3 +18,11 @@ systemctl enable jenkins.service
 Username: Admin
 Get Password Cmd: cd /var/lib/jenkins/secrets/,  and after nano initialAdminPassword
 #mvn cmd for war generation: mvn clean package
+#GETTING THE REPO (jenkins.io-->download --> ubuntu)
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
