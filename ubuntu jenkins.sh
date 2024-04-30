c8f96e28f1b1a4b8df724368bd18ecd5da03e62e
vjvikramgit <162918077+vjvikramgit@users.noreply.github.com>
2024-04-30 15:25:31 +0530

    Update ubuntu jenkins.sh

--- ubuntu jenkins.sh
+++ ubuntu jenkins.sh
@@ -2,14 +2,16 @@
 apt-get install openjdk-8-jdk
 #Install GIT and Maven
 apt-get install git maven
-#Install JAVA -11
-sudo apt-get install openjdk-11-jdk
+#Install latest java
+sudo apt install fontconfig openjdk-17-jre
 #GETTING THE REPO (jenkins.io-->download --> ubuntu)
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
 #Install Jenkins
 sudo apt-get install jenkins
 #JAVA switch user
